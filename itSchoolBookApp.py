d748c66963690e403ee5c4a6b59f6fa9adf64e6c
vasnei_razvan@live.com <Patratel91>
2021-07-16 16:13:20 +0300

    module split commit

--- itSchoolBookApp.py
+++ itSchoolBookApp.py
@@ -1,33 +1,10 @@
 import utilis
 
 
-def start_book():
-    #  with this function we will enter the date when we started
-    #  reading the book
-    start_date = input('When have you started to read the book? -> ')
-    import csv
-    with open('booksDB.csv', mode='r') as file:
-        rows = list(csv.DictReader(file, fieldnames=('BookName', 'AuthorName', 'SharedWith', 'IsRead',
-                                                     'StartDate', 'EndDate', 'Notes')))
-        for row in rows:
-            if row['BookName'] == 'BookName':
-                row['StartDate'] = start_date
-                break
-        with open('booksDB.csv', mode='w'):
-            csv_writer = csv.DictWriter(file, fieldnames=['BookName', 'AuthorName', 'SharedWith', 'IsRead',
-                                                          'StartDate', 'EndDate', 'Notes'])
-            csv_writer.writerow({'BookName': row.get('BookName'),
-                                 'AuthorName': row.get('AuthorName'),
-                                 'SharedWith': row.get('SharedWith'),
-                                 'IsRead': row.get('IsRead'),
-                                 'StartDate': start_date,
-                                 'EndDate': None})
-
-
 def end_book():
     #  with this function we will enter the date
     #  when we finished reading the book
-    end_date = input('When have you finished reading the book? -> ')
+
     import csv
     with open('booksDB.csv', mode='r') as file:
         rows = list(csv.DictReader(file, fieldnames=('BookName', 'AuthorName', 'SharedWith', 'IsRead',
--- update_book_module.py
+++ update_book_module.py
@@ -5,7 +5,6 @@ def update_book():
         book_read = True
     else:
         book_read = False
-    rows = []
     import csv
     with open('booksDB.csv', mode='r') as file:
         rows = list(csv.DictReader(file, fieldnames=('BookName', 'AuthorName', 'SharedWith', 'IsRead', 'StartDate',
@@ -22,3 +21,23 @@ def update_book():
                                      "SharedWith": row.get("SharedWith"),
                                      "IsRead": book_read})
         print("Book was updated successfully")
+        if book_read == "Y".upper():
+            start_date = input('When have you started to read the book? -> ')
+            end_date = input('When have you finished reading the book? -> ')
+            with open('booksDB.csv', mode='r') as isfile:
+                rows = list(csv.DictReader(isfile, fieldnames=('BookName', 'AuthorName', 'SharedWith', 'IsRead',
+                                                               'StartDate', 'EndDate', 'Notes')))
+                for row in rows:
+                    if row['BookName'] == 'BookName':
+                        row['StartDate'] = start_date
+                        row['EndDate'] = end_date
+                        break
+                with open('booksDB.csv', mode='w'):
+                    csv_writer = csv.DictWriter(file, fieldnames=['BookName', 'AuthorName', 'SharedWith', 'IsRead',
+                                                                  'StartDate', 'EndDate', 'Notes'])
+                    csv_writer.writerow({'BookName': row.get('BookName'),
+                                         'AuthorName': row.get('AuthorName'),
+                                         'SharedWith': row.get('SharedWith'),
+                                         'IsRead': row.get('IsRead'),
+                                         'StartDate': start_date,
+                                         'EndDate': end_date})
