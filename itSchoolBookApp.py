f93d979cef675ff55ba1d29bdc3431d6c8b764e6
vasnei_razvan@live.com <Patratel91>
2021-07-16 16:14:58 +0300

    last mod commit

--- itSchoolBookApp.py
+++ itSchoolBookApp.py
@@ -1,28 +1,2 @@
 import utilis
-
-
-def end_book():
-    #  with this function we will enter the date
-    #  when we finished reading the book
-
-    import csv
-    with open('booksDB.csv', mode='r') as file:
-        rows = list(csv.DictReader(file, fieldnames=('BookName', 'AuthorName', 'SharedWith', 'IsRead',
-                                                     'StartDate', 'EndDate', 'Notes')))
-        for row in rows:
-            if row['BookName'] == 'BookName':
-                row['EndDate'] = end_date
-                break
-        with open('booksDB.csv', mode='w'):
-            csv_writer = csv.DictWriter(file, fieldnames=['BookName', 'AuthorName', 'SharedWith', 'IsRead',
-                                                          'StartDate', 'EndDate', 'Notes'])
-            csv_writer.writerow({'BookName': row.get('BookName'),
-                                 'AuthorName': row.get('AuthorName'),
-                                 'SharedWith': row.get('SharedWith'),
-                                 'IsRead': row.get('IsRead'),
-                                 'StartDate': row.get('StartDate'),
-                                 'EndDate': row.get('EndDate'),
-                                 'Notes': row.get('Notes')})
-
-
 utilis.main_menu()
